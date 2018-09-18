b7a7a8035f2f54cf21cf56e3564812800af302ce
dfgarciag <diego.garciagarzon@students.olin.edu>
2018-09-18 16:46:43 -0400

    Updated wallfollowing

--- src/scripts/WallFollowingNode.py
+++ src/scripts/WallFollowingNode.py
@@ -14,7 +14,9 @@ import numpy as np
 def get_min_distance(values):
     values_np = np.asarray(values)
     values_np[values_np == 0] = np.nan # or use np.nan
-    return np.partition(values_np,2)[2]
+    ret_val = np.partition(values_np,2)[2]
+    print("get_min_distance", ret_val)
+    return ret_val
 
 class WallFollowingNode(object):
     """ This node moves forward at a fixed pace and stops when it detects an object within a certain distance. """
@@ -32,13 +34,16 @@ class WallFollowingNode(object):
         # variables from RotateNode.py file:
         # self.target_deg = 90
         self.kp = 0.3
+        self.smallest_distance_angle = None
 
         self.command = Twist()
 
 
     def process_scan(self, m):
         self.objdistance = get_min_distance(m.ranges)
-        self.angle = m.ranges.index(self.objdistance)
+        self.smallest_distance_angle = m.ranges.index(self.objdistance)
+        rospy.loginfo_throttle(0.5, "objdistance={} angle={}".format(self.objdistance, self.smallest_distance_angle))
+
         self.distances = m.ranges
 
 
@@ -83,21 +88,37 @@ class WallFollowingNode(object):
     #             rospy.loginfo_throttle(0.5, "target={} current={} error={}".format(target, math.degrees(self.current_rad), angleerror))
     #         r.sleep()
 
-    def orient_wall(self, ang_range=10, acceptableErrorDist = 0.1):
+    def orient_wall(self, ang_range=15, acceptableErrorDist = 0.1):
         """ Uses the distance between angles 90+30 and 90-30 to control the robot's orientation """
-        dist1 = self.distances[90+ang_range]
-        dist2 = self.distances[90-ang_range]
-        rospy.loginfo_throttle(0.5, "dist1={} dist2={} difference={}".format(dist1, dist2, disterror))
-        return
-        # while not rospy.is_shutdown():
-        #     disterror = dist1-dist2
-        #     if disterror > acceptableErrorDist:
-        #         self.command.angular.z = self.kp * disterror # proportional control of error
-        #         self.command.linear.x = 0
-        #         self.pub.publish(self.command)
-        #         # printing the message at 2Hz (once per 0.5 seconds)
-        #     else:
-        #         return
+        while not rospy.is_shutdown():
+            dist270_1 = self.distances[270+ang_range]
+            dist270_2 = self.distances[270-ang_range]
+            dist90_1 = self.distances[90+ang_range]
+            dist90_2 = self.distances[90+ang_range]
+            dist0_1 = self.distances[ang_range]
+            dist0_2 = self.distances[-ang_range]
+
+
+            if 0.0 in [dist0_1, dist0_2]:
+                print("OH NO!  MISSING DATA")
+
+            if dist0_1 > dist0_2:
+                disterror = dist270_1-dist270_2
+            else:
+                disterror = dist90_1 - dist90_2
+
+            print("disterror", disterror)
+            if abs(disterror) > acceptableErrorDist:
+                self.command.angular.z = -self.kp* disterror # proportional control of error
+                self.command.linear.x = 0.1
+                self.pub.publish(self.command)
+                # printing the message at 2Hz (once per 0.5 seconds)
+            else:
+                print("SUCCESSFULLY ORIENTED")
+                self.command.angular.z = 0
+                self.command.linear.x = 0.01
+                self.pub.publish(self.command)
+                return
 
 if __name__ == '__main__':
     node = WallFollowingNode(1)
